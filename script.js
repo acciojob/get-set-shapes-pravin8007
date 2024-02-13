cbfc3472590570adbbff6271321def18291ac44e
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-02-13 11:33:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(height,width){
+		this._height = height;
+		this._width = width ;
+	}
+	get height(){
+		return this._height ;
+	}
+	get width(){
+		return this._width ;
+	}
 
-class Square extends Animal {}
+	getArea(){
+		return this._width * this._height;
+	}
+}
+
+class Square extends Animal {
+	constructor(side){
+		super(side,side);
+	}
+	getPerimeter(){
+		return 4 * this._width ;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
